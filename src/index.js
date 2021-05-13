3f6302145b2e861f961a0703f7a00c30eeff7929
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-13 16:46:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ fetch("https://dog.ceo/api/breeds/list/all")
 .then(json => {
   for (const breed in json.message) {
     const newBreed = document.createElement("li");
-    newBreed.innerHTML = json.message[breed];
+    newBreed.innerHTML = breed;
     dogBreeds.append(newBreed);
   }
 }).then(
